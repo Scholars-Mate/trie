283fd4a1729d1e73884c671c0a0f55e862d38489
Christopher Wong <cwong164@gmail.com>
2016-09-09 09:54:37 -0500

    Changed the trie stucture to use an array

--- trie.h
+++ trie.h
@@ -4,32 +4,7 @@
 
 // struct used to store trie
 typedef struct trie{
-                 struct trie* a;
-                 struct trie* b;
-                 struct trie* c;
-                 struct trie* d;
-                 struct trie* e;
-                 struct trie* f;
-                 struct trie* g;
-                 struct trie* h;
-                 struct trie* i;
-                 struct trie* j;
-                 struct trie* k;
-                 struct trie* l;
-                 struct trie* m;
-                 struct trie* n;
-                 struct trie* o;
-                 struct trie* p;
-                 struct trie* q;
-                 struct trie* r;
-                 struct trie* s;
-                 struct trie* t;
-                 struct trie* u;
-                 struct trie* v;
-                 struct trie* w;
-                 struct trie* x;
-                 struct trie* y;
-                 struct trie* z;
+                 struct trie* letter[26];
 		 bool isWord;
          } trie;
 
